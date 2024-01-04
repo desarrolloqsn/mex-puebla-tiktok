16b0a9a195d8f586527a930db523d3918cb321cc
Alejandro Front <desarrollo@qsocialnow.com>
2024-01-04 13:40:29 -0300

    modifico user

--- src/componentes/Formularios/Login/index.jsx
+++ src/componentes/Formularios/Login/index.jsx
@@ -26,7 +26,7 @@ const FormLogin = () => {
    
       dispatch(loginSuccess(user));
       setRedirectToDashboard(true); // Establece el estado para redireccionar a "/dashboard"
-    } else if (username === 'qsnvzla' && password === 'qsnvzla2023') {
+    } else if (username === 'mexpuebla' && password === 'mexpuebla2023') {
       const user = {
         username: 'qsnvzla',
         roles: ['user'],
